0a34128a13ec6b02d17e55e0f8130af5d7e474e1
1U25CS009-ARASASELVAM <arasuselvam372@gmail.com>
2026-08-21 12:10:57 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,6 +1,6 @@
 create database collegeDB;
 use collegeDB;
-create table department(departmentID INTEGER(10) primary key,deparmentname VARCHAR(10),HOD VARCHAR(10)Not Null);
-desc department;
-insert deparment values(120,"computer science","jeyanthi");
+create table department(departmentid INTEGER(10) primary key,departmentname VARCHAR(20),HOD VARCHAR(20) NOT NULL);
+DESC department;
+insert department values(105,"computer science","jayanthi");
 select * from department;
