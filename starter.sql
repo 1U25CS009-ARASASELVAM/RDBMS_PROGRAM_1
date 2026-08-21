c89a614a261d3d6d22d7e12f4d15cee74bd484ea
1U25CS009-ARASASELVAM <arasuselvam372@gmail.com>
2026-08-21 11:26:11 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,7 +1,6 @@
 create datebase selvam;
 use selvam;
-create table department(departmentID integer(10) primary key,deparmentname varchar(10),HOD varchar(10)Not Null;
+create table department(departmentID integer(10) primary key,deparmentname varchar(10),HOD varchar(10)Not Null);
 desc department;
-inster deparment values("20","computer science","guru");
+inster deparment values(120,"computer science","jeyanthi");
 select*from deparment;
-desc deparment;
