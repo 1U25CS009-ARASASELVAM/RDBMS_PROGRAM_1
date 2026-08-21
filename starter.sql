7af71a4e40703cc0e974517f2a28144d69e06f55
1U25CS009-ARASASELVAM <arasuselvam372@gmail.com>
2026-08-21 11:21:49 +0530

    Update starter.sql

--- starter.sql
+++ starter.sql
@@ -1,1 +1,7 @@
-
+create datebase selvam;
+use selvam;
+create table department(departmentID integer(10) primary key,deparmentname varchar(10),HOD varchar(10)Not Null;
+desc department;
+inster deparment values("20","computer science","guru");
+select*from deparment;
+desc deparment;
